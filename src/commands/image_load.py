cfaf6cd19b5900e517276d4dbf38f25c93e82332
KoshakLoL <motorealinfo@gmail.com>
2021-08-13 19:11:16 +0500

    Fixed document and image loading

--- src/commands/image_load.py
+++ src/commands/image_load.py
@@ -1,6 +1,9 @@
 from vkbottle.tools import PhotoMessageUploader, DocMessagesUploader
 from vkbottle.api import API
+from os.path import abspath
 from random import randint
+from typing import List, Optional
+import re
 
 
 async def get_photo(photo_path: str, vk_api: API) -> str:
@@ -12,7 +15,7 @@ async def get_photo(photo_path: str, vk_api: API) -> str:
     :raises: TODO
     """
     msg_uploader: PhotoMessageUploader = PhotoMessageUploader(vk_api)
-    upload_str = await msg_uploader.upload(photo_path)
+    upload_str = await msg_uploader.upload(abspath(photo_path))
     return upload_str
 
 
@@ -26,7 +29,11 @@ async def get_document(document_path: str, vk_api: API, pid: int) -> str:
     :raises: TODO
     """
     msg_uploader: DocMessagesUploader = DocMessagesUploader(vk_api)
-    _, doctype = document_path.split(".")
-    upload_str = await msg_uploader.upload(f"dance-{randint(1, 999999)}.{doctype}",
-                                           document_path, peer_id=pid)
-    return upload_str
+    document_path = abspath(document_path)
+    doctype: Optional[re.Match] = re.search(r"(?:\/\w+\.)\w+$", document_path)
+    if doctype:
+        upload_name: str = f"dance{randint(1, 999999)}.{doctype.group()}"
+        upload_str = await msg_uploader.upload(upload_name, document_path, peer_id=pid)
+        return upload_str
+    else:
+        raise FileNotFoundError("No document file extension was found")
